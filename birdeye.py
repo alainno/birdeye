111dfac4245a9cd73970c4c5ea49da740005c17b
Alain <alain@gruposistemas.com>
2020-05-28 19:26:09 -0500

    frameview

--- birdeye.py
+++ birdeye.py
@@ -1,36 +1,50 @@
 import cv2
 import numpy as np
-from functions import getBirdView
+from functions import getBirdView, getIntegratedView
 
 vs = cv2.VideoCapture('TownCentreXVID.avi')
 
-w = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH)*0.3)
-h = int(vs.get(cv2.CAP_PROP_FRAME_HEIGHT)*0.3)
+w = vs.get(cv2.CAP_PROP_FRAME_WIDTH)
+h = vs.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-while True:
+
+#h = 400
+#w = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH)/vs.get(cv2.CAP_PROP_FRAME_HEIGHT))*h
+
+#quit()
+
+i=0
+
+while (True and i<100):
   
   (grab, frame) = vs.read()
 
   if not grab:
     break
 
-  frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_CUBIC)
+  #fy = 400/h
+  #fx = (w/h)*fy
+  fy = 400/h
+  frame = cv2.resize(frame, None, fx=fy, fy=fy, interpolation=cv2.INTER_CUBIC)
 
   #frame = cv2.resize(frame, (w+200,h))
   #print(frame.shape)
   #a = np.random.rand(N,N)
   #print(type(frame2))
-  frame2 = getBirdView([(2100,300),(2200,350)], [1,0], (200,h))
-  
-  frame3 = np.concatenate((frame, frame2), axis=1)
-  #print(frame.shape)
 
-  #integrated_view = getIntegratedView()
+  points = [(2100,300),(2200,350)]
+  colors = [1,0]
 
-  cv2.imshow("Tracking", frame3)
+  frame2 = getBirdView(points, colors, (200,400))
+
+  integrated_image = getIntegratedView(frame, frame2)
+
+  cv2.imshow("Distanciamiento Social", integrated_image)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
+
+  i+=1
 
 vs.release()
 cv2.destroyAllWindows()
--- functions.py
+++ functions.py
@@ -32,6 +32,43 @@ def getBirdView(points, colors, target_size):
   #frame2[:,:]=(255,0,0)
   #return frame2
 
+def __generate_partial_image(picture, partial_image, position, transparent=False):
+    """
+    Esta función genera incrusta una imagen parcial a nuestro picture final
+    Parámetros:
+        picture: Imagen que va a contener a las vistas parciales, es el marco principal de la aplicación. 
+                 Debe generarse una única vez, recomendable usa las dimensiones  (1250, 2600, 3). 
+                 Usar la función 'generate_picture' para obtener este parámetro.
+        partial_image: Imagen parcial que se incrustará en una posición dada dentro del picture
+        position: Posición de la imagen parcial
+    Salida:
+        Retorna la imagen del picture con la vista parcial incrustada
+    """
+    if not isinstance(position, tuple):
+        raise Exception("position must be a tuple representing x,y coordinates")
+    
+    image_height, image_width = partial_image.shape[:2]
+    x, y = position
+
+    if transparent:
+      foreground = partial_image[:,:]>0
+      #print("foreground")
+      #print(foreground)
+      #picture[x: x + image_height, y: y + image_width] = partial_image[foreground]
+      picture[x: x + image_height, y: y + image_width][foreground] = partial_image[foreground]
+    else:
+      picture[x: x + image_height, y: y + image_width] = partial_image
+
+def getIntegratedView(video_image, birdview_image):
+  picture = cv2.imread('frameview.png')
+  content = picture.copy()
+
+  __generate_partial_image(content, video_image, (100, 20))
+
+  __generate_partial_image(content, birdview_image, (100, 752), True)
+  
+  return content
+
 if __name__ == "__main__":
     bird_view = getBirdView([(2100,300)], [1], (200,400))
     cv2.imshow("hola", cv2.cvtColor(bird_view, cv2.COLOR_BGR2RGB))
